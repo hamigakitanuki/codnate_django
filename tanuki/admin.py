a1f581130065dc4c7171f911e88c8aae48a97853
はみがきたぬき <kakizaki0213@gmail.com>
2019-11-17 04:25:11 +0900

    a

--- tanuki/admin.py
+++ tanuki/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Photo,Account,BlackList
+from .models import Photo,Account,BlackList,Sub_type_value,Color_type_value,Codenate
 
 # Register your models here.
 admin.site.register(Photo)
 admin.site.register(Account)
 admin.site.register(BlackList)
+admin.site.register(Codenate)
+admin.site.register(Sub_type_value)
+admin.site.register(Color_type_value)
