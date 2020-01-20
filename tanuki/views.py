54c2fb8b3739f148427ebd2b38268f3f7f49637e
hamigakitanuki <kakizaki0213@gmail.com>
2020-01-20 11:27:07 +0900

    a

--- tanuki/views.py
+++ tanuki/views.py
@@ -1515,7 +1515,7 @@ def get_recomend_item_list(request):
     select_price = []
     for i in recomend_idx:
         select_link_url.append(link_url[i])
-        select_url.append(image_url[i])
+        select_image_url.append(image_url[i])
         select_sub.append(sub[i])
         select_price.append(price[i])
 
