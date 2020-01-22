eeb7f91997469628aa49a730e23cba828bf0b985
hamigakitanuki <kakizaki0213@gmail.com>
2020-01-22 14:27:41 +0900

    a

--- tanuki/admin.py
+++ tanuki/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Photo,Account,Codnate_sample,Photo_one,Codnate_type_temp,Bad_Codnate,Good_Codnate
+from .models import *
 
 # Register your models here.
 admin.site.register(Photo)
@@ -9,3 +9,6 @@ admin.site.register(Photo_one)
 admin.site.register(Codnate_type_temp)
 admin.site.register(Bad_Codnate)
 admin.site.register(Good_Codnate)
+
+admin.site.register(Local_shop)
+admin.site.register(Shop_photo)
